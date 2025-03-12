e8ed88509c4442002903fc9359fc3ef405f5ca82
Leandro Rodrigues <leandro.noisecall@gmail.com>
2025-03-12 10:37:41 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -37,7 +37,7 @@
 
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:[`{ disabled: "$var_all.cond1", onHoverIn: "$var_all.func1", children: "$var_all.Comp1" ,
-styles:{backgroundColor: 'green',
+style:{backgroundColor: 'green',
 }
 
 }`],
