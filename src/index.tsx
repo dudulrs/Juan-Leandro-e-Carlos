c7ee6830f57ff52eb358f0e8fdac2fa97701da90
Leandro Rodrigues <leandro.noisecall@gmail.com>
2025-03-12 10:52:26 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -240,7 +240,7 @@ backgroundColor: 'green',
 'lists': { 'lt1': [{name: "João", img: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},{name: "Luciana"},{name: "Pedro"}] } 
 , 
 'name': "Name:", 
-'dataToSet': {name: "Carlos", phone: "1111"}, 'no name': (x)=>console.log("oi") } 
+'dataToSet': {name: "Carlos", phone: "1111"}, 'func2': (x)=>console.log("oi") } 
  
 } 
  );
